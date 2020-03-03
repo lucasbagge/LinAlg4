394111ac0f7752eba89b3daa3850476f79f6bec7
lucasbagge <lucasbagge@hotmail.com>
2020-03-03 05:48:49 +0100

    adjusted gramSmith function so it can handle three vectors.

--- R/gramSmith.R
+++ R/gramSmith.R
@@ -4,6 +4,7 @@
 #'
 #' @param x1 The first vector.
 #' @param x2 The second vector.
+#' @param x3 The third vector.
 #'
 #' @return Should give us a number.
 #' @export
@@ -11,11 +12,22 @@
 #' x1 <- c(1,1,1,1,1,1)
 #' x2 <- c(1,1,1,1,1,1)
 #' gramSmith(x1,x2)
-gramSmith <- function(x1,x2){
+gramSmith <- function(x1,x2,x3){
+  if (missing(x3)){
   x2x1 <- t(x1) %*% x2
   x1x1 <- t(x1) %*% x1
   v1 <- x1
   v2 <- x2 - drop(x2x1/x1x1) * x1
-
   return(cbind(v1,v2))
+  } else {
+    x2x1 <- t(x2) %*% x1
+    x1x1 <- t(x1) %*% x1
+    v1 <- x1
+    v2 <- x2 - drop(x2x1/x1x1) * x1
+    x3v1 <- t(x3) %*% v1
+    x3v2 <- t(x3) %*% v2
+    v2v2 <- t(v2) %*% v2
+    v3 <- x3 - (drop(x3v1/x1x1) * x1) - (drop(x3v2/v2v2) * v2)
+    return(cbind(v1,v2,v3))
+  }
 }
